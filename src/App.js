22697b05d712ccd5c664ed5edfd49695e4c62281
andrewjwhite99 <andrew.j.white@bjss.com>
2022-11-18 12:12:41 +0000

    got the app sort of working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
 import Home from "./Pages/Home";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 function App() {
-  const client = new ApolloClient({
-    cache: new InMemoryCache(),
-    uri: "https://graphql-weather-api.herokuapp.com/",
-  });
+	const client = new ApolloClient({
+		cache: new InMemoryCache(),
+		uri: "https://graphql-weather-api.herokuapp.com/",
+	});
 
-  return (
-    <ApolloProvider client={client}>
-      <Home />
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Home />
+		</ApolloProvider>
+	);
 }
 
 export default App;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,47 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLazyQuery } from "@apollo/client";
 import { GET_WEATHER_QUERY } from "../graphql/Queries";
 
 function Home() {
-  const [citySearched, setCitySearched] = useState("");
-  const [getWeather, { data, error }] = useLazyQuery(GET_WEATHER_QUERY, {
-    variables: { name: citySearched },
-  });
+	const [getWeather, { loading, data, error }] = useLazyQuery(
+		GET_WEATHER_QUERY,
+		{
+			variables: { name: "Vancouver" },
+		}
+	);
 
-  if (error) return <h1> Error found</h1>;
+	if (error) return <h1>Error found...</h1>;
+	if (data) {
+		console.log(data);
+	}
 
-  if (data) {
-    console.log(data);
-  }
-
-  return (
-    <div className="home">
-      <h1>Search For Weather</h1>
-      <input
-        type="text"
-        placeholder="City name..."
-        onChange={(event) => {
-          setCitySearched(event.target.value);
-        }}
-      />
-      <button onClick={() => getWeather()}> Search</button>
-      <div className="weather">
-        {data && (
-          <>
-            <h1> {data.getCityByName.name} </h1>
-            <h1>
-              {" "}
-              Temperature: {data.getCityByName.weather.temperature.actual}
-            </h1>
-            <h1>
-              Description: {data.getCityByName.weather.summary.description}
-            </h1>
-            <h1>Wind Speed: {data.getCityByName.weather.wind.speed}</h1>
-          </>
-        )}
-      </div>
-    </div>
-  );
+	return (
+		<div className="home">
+			<h1>Search for weather</h1>
+			<input type="text" placeholder="City name..." />
+			<button onClick={() => getWeather()}>Search</button>
+		</div>
+	);
 }
 
 export default Home;
